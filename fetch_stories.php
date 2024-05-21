78bd6e2c4d2bbc746c477e83ce4a06853ff5c8a4
AKR-MODSS <goldp3rl@gmail.com>
2024-05-21 23:30:06 +0530

    Add files via upload

---
+++ fetch_stories.php
@@ -0,0 +1,6 @@
+<?php
+$username = $_GET['username'];
+// Use Instagram API to fetch stories for the given username
+// Implement this function using Instagram API
+// Return JSON response containing story data
+?>
